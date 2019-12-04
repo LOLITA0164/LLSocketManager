1c9f98d7c18b7b37a49b4f63e42a3674410ad6b6
LOLITA0164 <476512340@qq.com>
2019-12-04 15:39:17 +0800

    Initial commit

---
+++ Example/LLSocketManager/LLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LLAppDelegate.h
+//  LLSocketManager
+//
+//  Created by LOLITA0164 on 12/04/2019.
+//  Copyright (c) 2019 LOLITA0164. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LLSocketManager/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LLSocketManager
+//
+//  Created by LOLITA0164 on 12/04/2019.
+//  Copyright (c) 2019 LOLITA0164. All rights reserved.
+//
+
+@import UIKit;
+#import "LLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LLAppDelegate class]));
+    }
+}
